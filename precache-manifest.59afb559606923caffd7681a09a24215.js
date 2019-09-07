dc9e85a7403c2f3d455e7f9e99d313566678cd52
didolf <didolf@yandex-team.ru>
2019-09-07 16:53:40 +0300

    Updates

---
+++ precache-manifest.59afb559606923caffd7681a09a24215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ed1e63735117f8b0a551e5ed56d6f8",
+    "url": "/pizzaprocloud-ui/index.html"
+  },
+  {
+    "revision": "46728c2e5a0af91fd115",
+    "url": "/pizzaprocloud-ui/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "079de2f05f554d2822b1",
+    "url": "/pizzaprocloud-ui/static/css/main.90280c45.chunk.css"
+  },
+  {
+    "revision": "46728c2e5a0af91fd115",
+    "url": "/pizzaprocloud-ui/static/js/2.28de4116.chunk.js"
+  },
+  {
+    "revision": "079de2f05f554d2822b1",
+    "url": "/pizzaprocloud-ui/static/js/main.a9d72525.chunk.js"
+  },
+  {
+    "revision": "5036f83f9403706e8e0a",
+    "url": "/pizzaprocloud-ui/static/js/runtime~main.a538e187.js"
+  }
+]);
