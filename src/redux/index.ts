8d16d0a97fa44c395066fea43292f662b0b482f7
didolf <didolf@yandex-team.ru>
2019-10-10 09:45:34 +0300

    chore: Добавил сброс localStorage

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,12 +1,19 @@
 import { combineReducers, createStore } from 'redux';
-import { languages, user, formDataState, cartProducts, settings } from './reducers';
+import {
+  languages, user, formDataState, cartProducts, settings,
+} from './reducers';
 
-export const appState = combineReducers({ languages, user, formDataState, cartProducts, settings } as any);
-const testIsNew = localStorage.getItem('testIndex') && JSON.parse(localStorage.getItem('testIndex') as string);
+export const appState = combineReducers({
+  languages,
+  user,
+  formDataState,
+  cartProducts,
+  settings,
+} as any);
+const testIsNew = localStorage.getItem('testIndex1') && JSON.parse(localStorage.getItem('testIndex') as string);
 const initialState = testIsNew ? JSON.parse(localStorage.getItem('pizza-redux') as string) : null;
-let store = initialState ? createStore(appState, initialState) : createStore(appState);
-localStorage.setItem('testIndex', JSON.stringify(true));
+const store = initialState ? createStore(appState, initialState) : createStore(appState);
+localStorage.setItem('testIndex1', JSON.stringify(true));
 store.subscribe(() => localStorage.setItem('pizza-redux', JSON.stringify(store.getState())));
 
 export default store;
-
